b690ddafb35d10404338cca0ddfa5809e4908727
gugupy <gugu.ap900@gmail.com>
2025-11-10 23:21:54 +0530

    Updated files

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -1,300 +1,268 @@
-import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/types";
-import { Line, Logo, Row, Text } from "@once-ui-system/core";
+import {
+    About,
+    Blog,
+    Gallery,
+    Home,
+    Newsletter,
+    Person,
+    Social,
+    Work,
+} from "@/types";
+import { InlineCode, Line, Logo, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
-  avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+    firstName: "Gughanathan",
+    lastName: "Mani",
+    name: `Gugu`,
+    role: "Python Developer",
+    avatar: "/images/avatar.jpg",
+    email: "gugu.ap900@gmail.com",
+    location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    languages: ["Tamil", "English", "Kannata"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
-  display: true,
-  title: <>Subscribe to {person.firstName}'s Newsletter</>,
-  description: <>My weekly newsletter about creativity and engineering</>,
+    display: false,
+    title: <>Subscribe to {person.firstName}'s Newsletter</>,
+    description: <>My weekly newsletter about creativity and engineering</>,
 };
 
 const social: Social = [
-  // Links are automatically displayed.
-  // Import new icons in /once-ui/icons.ts
-  {
-    name: "GitHub",
-    icon: "github",
-    link: "https://github.com/once-ui-system",
-  },
-  {
-    name: "LinkedIn",
-    icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
-  },
-  {
-    name: "Threads",
-    icon: "threads",
-    link: "https://www.threads.com/@once_ui",
-  },
-  {
-    name: "Email",
-    icon: "email",
-    link: `mailto:${person.email}`,
-  },
+    // Links are automatically displayed.
+    // Import new icons in /once-ui/icons.ts
+    {
+        name: "GitHub",
+        icon: "github",
+        link: "https://github.com/gugupy",
+    },
+    {
+        name: "LinkedIn",
+        icon: "linkedin",
+        link: "https://www.linkedin.com/in/gughanathan-m-079913171/",
+    },
+    {
+        name: "X",
+        icon: "twitter",
+        link: "https://x.com/ManiGughan",
+    },
+    {
+        name: "Threads",
+        icon: "threads",
+        link: "https://www.threads.com/@gugu_mani",
+    },
+    {
+        name: "Email",
+        icon: "email",
+        link: `mailto:${person.email}`,
+    },
 ];
 
 const home: Home = {
-  path: "/",
-  image: "/images/og/home.jpg",
-  label: "Home",
-  title: `${person.name}'s Portfolio`,
-  description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between design and code</>,
-  featured: {
-    display: true,
-    title: (
-      <Row gap="12" vertical="center">
-        <strong className="ml-4">Once UI</strong>{" "}
-        <Line background="brand-alpha-strong" vert height="20" />
-        <Text marginRight="4" onBackground="brand-medium">
-          Featured work
-        </Text>
-      </Row>
+    path: "/",
+    image: "",
+    label: "Home",
+    title: `${person.name}'s Portfolio`,
+    description: `Portfolio website showcasing my work as a ${person.role}`,
+    headline: <>Transforming Ideas into Scalable Systems</>,
+    featured: {
+        display: true,
+        title: (
+            <Row gap="12" vertical="center">
+                <strong className="ml-4">Gugu's</strong>{" "}
+                <Line background="brand-alpha-strong" vert height="20" />
+                <Text marginRight="4" onBackground="brand-medium">
+                    Featured work
+                </Text>
+            </Row>
+        ),
+        href: "https://github.com/gugupy/adminjs-keycloak",
+    },
+    subline: (
+        <>
+            Python developer at <InlineCode>BI2Value GmbH</InlineCode>, crafting
+            Flask and AI-driven apps.
+        </>
     ),
-    href: "/work/building-once-ui-a-customizable-design-system",
-  },
-  subline: (
-    <>
-      I'm Selene, a design engineer at{" "}
-      <Logo
-        dark
-        icon="/trademarks/wordmark-dark.svg"
-        style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}
-      />
-      , where I craft intuitive
-      <br /> user experiences. After hours, I build my own projects.
-    </>
-  ),
 };
 
 const about: About = {
-  path: "/about",
-  label: "About",
-  title: `About – ${person.name}`,
-  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
-  tableOfContent: {
-    display: true,
-    subItems: false,
-  },
-  avatar: {
-    display: true,
-  },
-  calendar: {
-    display: true,
-    link: "https://cal.com",
-  },
-  intro: {
-    display: true,
-    title: "Introduction",
-    description: (
-      <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
-      </>
-    ),
-  },
-  work: {
-    display: true, // set to false to hide this section
-    title: "Work Experience",
-    experiences: [
-      {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
-        achievements: [
-          <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
-          </>,
-          <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
-          </>,
-        ],
-        images: [
-          // optional: leave the array empty if you don't want to display images
-          {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
-            width: 16,
-            height: 9,
-          },
-        ],
-      },
-      {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
-        achievements: [
-          <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
-          </>,
-          <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
-          </>,
-        ],
-        images: [],
-      },
-    ],
-  },
-  studies: {
-    display: true, // set to false to hide this section
-    title: "Studies",
-    institutions: [
-      {
-        name: "University of Jakarta",
-        description: <>Studied software engineering.</>,
-      },
-      {
-        name: "Build the Future",
-        description: <>Studied online marketing and personal branding.</>,
-      },
-    ],
-  },
-  technical: {
-    display: true, // set to false to hide this section
-    title: "Technical skills",
-    skills: [
-      {
-        title: "Figma",
+    path: "/about",
+    label: "About",
+    title: `About – ${person.name}`,
+    description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+    tableOfContent: {
+        display: true,
+        subItems: false,
+    },
+    avatar: {
+        display: true,
+    },
+    calendar: {
+        display: true,
+        link: "https://teams.microsoft.com/l/meeting/new",
+    },
+    freelancer: {
+        display: true,
+        link: "https://www.freelancer.in/hireme/guguap900",
+    },
+    intro: {
+        display: true,
+        title: "Introduction",
         description: (
-          <>Able to prototype in Figma with Once UI with unnatural speed.</>
+            <>
+                I'm a freelance Python developer passionate about open source,
+                specializing in Superset and Flask-AppBuilder for data-driven
+                applications. I work across Azure, AWS, and GCP to build
+                scalable solutions, and I’m expanding my expertise in ReactJS
+                and frontend development while continuing to strengthen my
+                backend skills.
+            </>
         ),
-        tags: [
-          {
-            name: "Figma",
-            icon: "figma",
-          },
+    },
+    work: {
+        display: true, // set to false to hide this section
+        title: "Work Experience",
+        experiences: [
+            {
+                company: "BI2Value GmBH",
+                timeframe: "2023 - Present",
+                role: "Python Developer/DevOps Engineer",
+                achievements: [
+                    <>Customize Apache superset and maintain.</>,
+                    <>Developing Phelina a powerful AI for Data insights.</>,
+                    <>
+                        Create and maintain the Airflow DAGs for the data
+                        pipeline.
+                    </>,
+                    <>Maintain infrastructure on Azure and AWS.</>,
+                ],
+                images: [
+                    {
+                        src: "/images/projects/qmantic/phelina-01.gif",
+                        alt: "Phelina MSTeams Bot",
+                        width: 16,
+                        height: 9,
+                    },
+                ],
+            },
+            {
+                company: "ITC Infotech India Ltd",
+                timeframe: "2019 - 2023",
+                role: "Linux Administrator/Azure Cloud Engineer",
+                achievements: [
+                    <>
+                        Managed and maintained Linux servers and Azure cloud
+                        infrastructure.
+                    </>,
+                    <>Setup complete infrastructure for the new projects.</>,
+                ],
+                images: [],
+            },
+            {
+                company: "Fractalio Data Pvt Ltd",
+                timeframe: "2017 - 2019",
+                role: "Linux Administrator",
+                achievements: [
+                    <>Managed and maintained Linux servers on client site.</>,
+                    <>Monitor daily and monthly backup of the servers.</>,
+                ],
+                images: [],
+            },
         ],
-        // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+    },
+    studies: {
+        display: true, // set to false to hide this section
+        title: "Studies",
+        institutions: [
+            {
+                name: "Anna University",
+                description: <>Studied software engineering.</>,
+            },
         ],
-      },
-      {
-        title: "Next.js",
-        description: (
-          <>Building next gen apps with Next.js + Once UI + Supabase.</>
-        ),
-        tags: [
-          {
-            name: "JavaScript",
-            icon: "javascript",
-          },
-          {
-            name: "Next.js",
-            icon: "nextjs",
-          },
-          {
-            name: "Supabase",
-            icon: "supabase",
-          },
+    },
+    technical: {
+        display: true, // set to false to hide this section
+        title: "Technical skills",
+        skills: [
+            {
+                title: "Languages",
+                description: <></>,
+                tags: [
+                    { name: "Python", icon: "python" },
+                    { name: "Rust", icon: "rust" },
+                    { name: "Javascript", icon: "javascript" },
+                    { name: "TypeScript", icon: "typescript" },
+                ],
+                // optional: leave the array empty if you don't want to display images
+                images: [],
+            },
+            {
+                title: "Cloud Infrastructure",
+                description: <></>,
+                // optional: leave the array empty if you don't want to display images
+                tags: [
+                    { name: "Azure", icon: "azure" },
+                    { name: "AWS", icon: "aws" },
+                    { name: "GCP", icon: "gcp" },
+                ],
+                images: [],
+            },
+            {
+                title: "Infrastructure as Code (IaC)",
+                description: <></>,
+                // optional: leave the array empty if you don't want to display images
+                tags: [
+                    { name: "Pulumi", icon: "pulumi" },
+                    { name: "Terraform", icon: "terraform" },
+                ],
+                images: [],
+            },
         ],
-        // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
-      },  
-    ],
-  },
+    },
 };
 
 const blog: Blog = {
-  path: "/blog",
-  label: "Blog",
-  title: "Writing about design and tech...",
-  description: `Read what ${person.name} has been up to recently`,
-  // Create new blog posts by adding a new .mdx file to app/blog/posts
-  // All posts will be listed on the /blog route
+    path: "/blog",
+    label: "Blog",
+    title: "Writing about technology",
+    description: `Read what ${person.name} has been up to recently`,
+    // Create new blog posts by adding a new .mdx file to app/blog/posts
+    // All posts will be listed on the /blog route
 };
 
 const work: Work = {
-  path: "/work",
-  label: "Work",
-  title: `Projects – ${person.name}`,
-  description: `Design and dev projects by ${person.name}`,
-  // Create new project pages by adding a new .mdx file to app/blog/posts
-  // All projects will be listed on the /home and /work routes
+    path: "/work",
+    label: "Work",
+    title: `Projects – ${person.name}`,
+    description: `Design and dev projects by ${person.name}`,
+    // Create new project pages by adding a new .mdx file to app/blog/posts
+    // All projects will be listed on the /home and /work routes
 };
 
 const gallery: Gallery = {
-  path: "/gallery",
-  label: "Gallery",
-  title: `Photo gallery – ${person.name}`,
-  description: `A photo collection by ${person.name}`,
-  // Images by https://lorant.one
-  // These are placeholder images, replace with your own
-  images: [
-    {
-      src: "/images/gallery/horizontal-1.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/vertical-4.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/horizontal-3.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/vertical-1.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/vertical-2.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/horizontal-2.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/horizontal-4.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/vertical-3.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-  ],
+    path: "/gallery",
+    label: "Gallery",
+    title: `Photo gallery – ${person.name}`,
+    description: `A photo collection by ${person.name}`,
+    // Images by https://lorant.one
+    // These are placeholder images, replace with your own
+    images: [
+        {
+            src: "/images/gallery/hori-01.jpg",
+            alt: "image",
+            orientation: "horizontal",
+        },
+        {
+            src: "/images/gallery/vert-01.jpg",
+            alt: "image",
+            orientation: "vertical",
+        },
+        {
+            src: "/images/gallery/vert-02.jpg",
+            alt: "image",
+            orientation: "vertical",
+        },
+    ],
 };
 
 export { person, social, newsletter, home, about, blog, work, gallery };
